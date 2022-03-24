bb082cabbdd611bdf1747edb52e99f48e5c8395b
Md Kibria <mdkibria684@gmail.com>
2022-03-24 11:37:16 +0600

    Add no todo message

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,17 +1,26 @@
 import AddTodo from "./AddTodo"
 import TodoItem from "./TodoItem"
 
-const Container = ({todos}) => {
+const Container = ({ todos }) => {
 
     return (
         <div className='container'>
             <h1>Todo App</h1>
             <AddTodo />
-            <div className="todos">
-                {todos.map((todo, index) => (
-                    <TodoItem key={todo._id} title={todo.title} id={todo._id} index={index} />
-                ))}
-            </div>
+            {todos.length === 0
+                ? (
+                    <p className="no-todos">
+                        No Todos
+                    </p>
+                )
+                : (
+                    <div className="todos">
+                        {todos.map((todo, index) => (
+                            <TodoItem key={todo._id} title={todo.title} id={todo._id} index={index} />
+                        ))}
+                    </div>
+                )}
+
         </div>
     )
 }
